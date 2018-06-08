4090d91d5632cf14deece29ad4ccc32adef80ddd
Reigningsun <38302744+Reigningsun@users.noreply.github.com>
2018-06-08 16:09:17 -0500

    Update Defending2.java
    Reformatting for Github

--- Defending2.java
+++ Defending2.java
@@ -13,14 +13,14 @@ public class Defending2 implements State2 {
 	
 	@Override
 	public void move() {
-		Point bestMove = pirate2.getBestMove(treasurePos);										// === Move as quickly as possible to be near the treasure =============================================
+		Point bestMove = pirate2.getBestMove(treasurePos);						// === Move as quickly as possible to be near the treasure =============================================
 		pirate2.piratePos.setLocation(bestMove.getX(), bestMove.getY());
 		
 	}
 
 	@Override
 	public void lookForPlayer() {
-		if (pirate2.canSeePlayer() && pirate2.timeSincePlayerLastSeen >= 2){					// === Chase the player if it is still close to the pirate after it has moved toward the treasure ======
+		if (pirate2.canSeePlayer() && pirate2.timeSincePlayerLastSeen >= 2){				// === Chase the player if it is still close to the pirate after it has moved toward the treasure ======
 			pirate2.timeSincePlayerLastSeen = 0;
 			State2 chasing = new Chasing2(pirate2);
 			pirate2.setState(chasing);
